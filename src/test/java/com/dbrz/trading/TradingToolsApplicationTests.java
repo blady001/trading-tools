a92285f9597c79cbfab4de93433906c6d65ddbeb
Dawid Brzyszcz <dawid.brzyszcz@hotmail.com>
2022-01-06 18:36:05 +0100

    Initial commit

---
+++ src/test/java/com/dbrz/trading/TradingToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbrz.trading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingToolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
