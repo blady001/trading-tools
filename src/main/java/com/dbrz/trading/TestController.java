bb07422c7a0214711451655e56ebe24ca3101957
Dawid Brzyszcz <dawid.brzyszcz@hotmail.com>
2022-01-06 18:41:30 +0100

    verified that app works

---
+++ src/main/java/com/dbrz/trading/TestController.java
@@ -0,0 +1,13 @@
+package com.dbrz.trading;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class TestController {
+
+    @GetMapping("/test")
+    String testEndpoint() {
+        return "Hello, it's working!";
+    }
+}
