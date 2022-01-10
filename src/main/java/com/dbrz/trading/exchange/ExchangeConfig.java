0e596f189339516da253e927c616707fa5b538d8
Dawid Brzyszcz <dawid.brzyszcz@hotmail.com>
2022-01-10 21:43:33 +0100

    minor refactor of ExchangeConfig.java

--- src/main/java/com/dbrz/trading/exchange/ExchangeConfig.java
+++ src/main/java/com/dbrz/trading/exchange/ExchangeConfig.java
@@ -2,15 +2,11 @@ package com.dbrz.trading.exchange;
 
 import com.binance.api.client.BinanceApiClientFactory;
 import com.binance.api.client.BinanceApiRestClient;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@RequiredArgsConstructor
 class ExchangeConfig {
-
-    private ExchangeConfig exchangeConfig;
 
     @Bean
     BinanceApiRestClient binanceApiRestClient() {
