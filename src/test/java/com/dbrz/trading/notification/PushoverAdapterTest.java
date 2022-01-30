ac24ca7050cea903a32b4b5c9a7ac7e56ab3e527
Dawid Brzyszcz <dawid.brzyszcz@hotmail.com>
2022-01-30 17:02:02 +0100

    replaced mockwebserver with wiremock

--- src/test/java/com/dbrz/trading/notification/PushoverAdapterTest.java
+++ src/test/java/com/dbrz/trading/notification/PushoverAdapterTest.java
@@ -1,7 +1,6 @@
 package com.dbrz.trading.notification;
 
-import okhttp3.mockwebserver.MockResponse;
-import okhttp3.mockwebserver.MockWebServer;
+import com.github.tomakehurst.wiremock.WireMockServer;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -9,47 +8,46 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.web.server.ResponseStatusException;
 import reactor.test.StepVerifier;
 
-import java.io.IOException;
-
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
 
 @ExtendWith(MockitoExtension.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class PushoverAdapterTest {
 
-    private MockWebServer mockWebServer;
+    private WireMockServer wireMockServer;
     private PushoverAdapter pushoverAdapter;
 
     @Mock
     private NotificationProperties.PushoverProperties pushoverProperties;
 
-    private static final String API_KEY_MOCK = "apiKey";
-    private static final String USER_KEY_MOCK = "userKey";
+    private static final String API_KEY = "apiKey";
+    private static final String USER_KEY = "userKey";
+    private static final int TEST_PORT = 8090;
 
     @BeforeAll
-    void setUp() throws IOException {
-        mockWebServer = new MockWebServer();
-        mockWebServer.start();
+    void setUp() {
+        wireMockServer = new WireMockServer(TEST_PORT);
+        wireMockServer.start();
     }
 
     @BeforeEach
     void init() {
-        when(pushoverProperties.getApiUrl()).thenReturn(String.format("http://localhost:%s", mockWebServer.getPort()));
-        when(pushoverProperties.getApiKey()).thenReturn(API_KEY_MOCK);
-        when(pushoverProperties.getUserKey()).thenReturn(USER_KEY_MOCK);
+        when(pushoverProperties.getApiUrl()).thenReturn(String.format("http://localhost:%s", TEST_PORT));
+        when(pushoverProperties.getApiKey()).thenReturn(API_KEY);
+        when(pushoverProperties.getUserKey()).thenReturn(USER_KEY);
         var notificationProperties = new NotificationProperties();
         notificationProperties.setPushoverProperties(pushoverProperties);
         pushoverAdapter = new PushoverAdapter(notificationProperties);
     }
 
     @AfterAll
-    void tearDown() throws IOException {
-        mockWebServer.shutdown();
+    void tearDown() {
+        wireMockServer.stop();
     }
 
     @Test
-    void shouldSendRequestToPushoverAndHandleSuccessfulResponse() throws InterruptedException {
+    void shouldSendRequestToPushoverAndHandleSuccessfulResponse() {
         // given
         var givenMessage = "Message";
         var givenNotification = new Notification("Title", givenMessage);
@@ -64,7 +62,7 @@ class PushoverAdapterTest {
     }
 
     @Test
-    void shouldReturnException() throws InterruptedException {
+    void shouldReturnException() {
         // given
         var givenMessage = "Message";
         var givenNotification = new Notification("Title", givenMessage);
@@ -81,21 +79,21 @@ class PushoverAdapterTest {
     }
 
     private void givenSuccessfulPushoverResponse() {
-        mockWebServer.enqueue(new MockResponse()
-                .setResponseCode(200)
-                .setBody("{\"status\": 1, \"request\": \"test\"}")
-                .addHeader("Content-Type", "application/json"));
+        wireMockServer.stubFor(post(urlPathEqualTo("/"))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withBody("{\"status\": 1, \"request\": \"test\"}")
+                        .withHeader("Content-Type", "application/json")));
     }
 
     private void givenUnsuccessfulPushoverResponse() {
-        mockWebServer.enqueue(new MockResponse()
-                .setResponseCode(500));
+        wireMockServer.stubFor(post(urlPathEqualTo("/"))
+                .willReturn(aResponse()
+                        .withStatus(500)));
     }
 
-    private void verifyPushoverCalled(String message) throws InterruptedException {
-        var recordedRequest = mockWebServer.takeRequest();
-        assertThat(recordedRequest.getMethod()).isEqualTo("POST");
-        assertThat(recordedRequest.getPath()).isEqualTo(
-                String.format("/?token=%s&user=%s&message=%s", API_KEY_MOCK, USER_KEY_MOCK, message));
+    private void verifyPushoverCalled(String message) {
+        var expectedPath = String.format("/?token=%s&user=%s&message=%s", API_KEY, USER_KEY, message);
+        wireMockServer.verify(postRequestedFor(urlEqualTo(expectedPath)));
     }
 }
